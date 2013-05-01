5b2110c76776a9d61cea2e3cb3e6dba0a3327b0e
Alex Smith <alex.geoffrey.smith@gmail.com>
2013-05-01 21:01:22 +1000

    can preview multipart mail

--- app/controllers/letter_bomb/mailers_controller.rb
+++ app/controllers/letter_bomb/mailers_controller.rb
@@ -1,13 +1,39 @@
 module LetterBomb
   class MailersController < ApplicationController
+
     def index
       @mailers = LetterBomb::Preview.previews
     end
 
     def show
-      mailer_class = params[:mailer_class]
-      mailer_action = params[:mailer_action]
-      @mail = mailer_class.constantize.preview_action(mailer_action)
+      klass = params[:mailer_class]
+      @action = params[:mailer_action]
+      @mail = klass.constantize.preview_action(@action)
+
+      respond_to do |format|
+        format.html
+        format.text { render formats: [:html], content_type: 'text/html' }
+      end
     end
+
+    private
+
+    def body_part
+      return @mail unless @mail.multipart?
+
+      content_type = Rack::Mime.mime_type(".#{params[:format]}")
+      if @mail.respond_to?(:all_parts)
+        @mail.all_parts.find { |part| part.content_type.match(content_type) } || @mail.parts.first
+      else
+        @mail.parts.find { |part| part.content_type.match(content_type) } || @mail.parts.first
+      end
+    end
+    helper_method :body_part
+
+    def html_template?
+      params[:format] == 'html'
+    end
+    helper_method :html_template?
+
   end
 end
